d38d4138f29d700c856519477b16ac53ef30766a
EVERTONROGERIONUNES <70280660+EVERTONROGERIONUNES@users.noreply.github.com>
2023-07-03 22:36:55 -0300

    create user.schema and conection mongodb

--- src/server.js
+++ src/server.js
@@ -1,11 +1,64 @@
+import 'dotenv/config';
 import express from 'express';
-const app = express()
-const port = 3000
+import UserService from '../src/services/user.service.js';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!!!!')
-})
+    res.send('ImagineShop API');
+});
+
+// C - CREATE
+app.post('/users', async (req, res) => {
+    const { name, email, password } = req.body;
+    const userService = new UserService();
+    await userService.add(name, email, password);
+    return res.status(201).json({ message: 'success' });
+});
+
+// R - READ
+app.get('/users', async (req, res) => {
+    const userService = new UserService();
+    const users = await userService.findAll();
+    return res.status(200).json(users);
+});
+
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const userService = new UserService();
+    const user = await userService.findById(id);
+    return res.status(200).json(user);
+});
+
+// U - UPDATE
+app.put('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const { name, email, password } = req.body;
+    const user = { name, email, password };
+    const userService = new UserService();
+    try {
+        await userService.update(id, user);
+        return res.status(200).json({ message: 'success' });
+    } catch (error) {
+        return res.status(404).json({ message: error.message })
+    }
+});
+
+// D - DELETE
+app.delete('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const userService = new UserService();
+    try {
+        await userService.delete(id);
+        return res.status(200).json({ message: 'success' });
+    } catch (error) {
+        return res.status(404).json({ message: error.message });
+    }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server running at http://localhost:${port}`);
+});
